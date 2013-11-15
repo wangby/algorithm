5280eb7fd634fe20c49bc4ee5d6277629e18f0bc
wangbiyao <wangbiyao@gmail.com>
2013-11-15 17:18:08 +0800

    ranklib

---
+++ ranklib-v2.1/run1.sh
@@ -0,0 +1,2 @@
+#! /bin/sh
+java -jar bin/RankLib.jar -train MQ2008/Fold1/train.txt -test MQ2008/Fold1/test.txt -validate MQ2008/Fold1/vali.txt -ranker 6 -metric2t NDCG@10 -metric2T ERR@10 -save mymodel.txt
