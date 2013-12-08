a50d09f40457f86e6cc69a4bcfe50f7d9ccbb2be
wangby <wangbiyao@gmail.com>
2013-12-08 11:55:16 +0800

    Update linear.h

--- liblinear-1.93/linear.h
+++ liblinear-1.93/linear.h
@@ -11,12 +11,26 @@ struct feature_node
 	double value;
 };
 
+struct sample_node
+{
+	int clicks;
+	int shows;
+	int start_index;
+	int end_index;
+};
+
 struct problem
 {
 	int l, n;
 	double *y;
 	struct feature_node **x;
 	double bias;            /* < 0 if no bias term */  
+};
+
+struct sample
+{
+	int l;
+	struct sample_node *s;
 };
 
 enum { L2R_LR, L2R_L2LOSS_SVC_DUAL, L2R_L2LOSS_SVC, L2R_L1LOSS_SVC_DUAL, MCSVM_CS, L1R_L2LOSS_SVC, L1R_LR, L2R_LR_DUAL, L2R_L2LOSS_SVR = 11, L2R_L2LOSS_SVR_DUAL, L2R_L1LOSS_SVR_DUAL }; /* solver_type */
